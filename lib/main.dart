4c2f67f04ab047cb36e98a8426265fda72d0c62c
Abhijeet Singh <109523390+AbhijeetS29@users.noreply.github.com>
2024-01-15 22:17:18 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
@@ -27,9 +37,39 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _controller = TextEditingController();
+
+
+  var color1 = Color(0xffe6f2ff);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: color1,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: "Enter Your Name",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orangeAccent),
+                  borderRadius: BorderRadius.circular(10.0), // Set the border radius to 0.0 for a square border
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
   }
 }
