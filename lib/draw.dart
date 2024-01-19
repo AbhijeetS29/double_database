92729c398a4801b3be66b701d09f56934629f729
Abhijeet Singh <109523390+AbhijeetS29@users.noreply.github.com>
2024-01-19 23:33:54 +0530

    update

--- lib/draw.dart
+++ lib/draw.dart
@@ -1,82 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/material.dart';
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _controller = TextEditingController();
-
-
-  var color1 = Color(0xffe6f2ff);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: color1,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: "Enter Your Name",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.orangeAccent),
-                  borderRadius: BorderRadius.circular(10.0), // Set the border radius to 0.0 for a square border
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-
-  }
-  void dispose(){
-    _controller.dispose();
-    super.dispose();
-  }
-}
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:mss_student_wing/main.dart';
+
+import 'main.dart';
 
 
 class HomePage extends StatefulWidget {
