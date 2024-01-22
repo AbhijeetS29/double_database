4a4a47e5165a651e9fee6e83d906e18ae777628e
Abhijeet Singh <109523390+AbhijeetS29@users.noreply.github.com>
2024-01-22 23:39:21 +0530

    FlutterMojis

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:fluttermoji/fluttermoji.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,51 +28,129 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home:  MyHomePage(),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _controller = TextEditingController();
-
-
-  var color1 = Color(0xffe6f2ff);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: color1,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: <Widget>[
+          SizedBox(
+            height: 25,
+          ),
           Padding(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: "Enter Your Name",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.orangeAccent),
-                  borderRadius: BorderRadius.circular(10.0), // Set the border radius to 0.0 for a square border
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "Use your Fluttermoji anywhere\nwith the below widget",
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          FluttermojiCircleAvatar(
+            backgroundColor: Colors.grey[200],
+            radius: 100,
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "and create your own page to customize them using our widgets",
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            children: [
+              Spacer(flex: 2),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  height: 35,
+                  child: ElevatedButton.icon(
+                    icon: Icon(Icons.edit),
+                    label: Text("Customize"),
+                    onPressed: () => Navigator.push(context,
+                        new MaterialPageRoute(builder: (context) => NewPage())),
+                  ),
                 ),
               ),
-            ),
+              Spacer(flex: 2),
+            ],
+          ),
+          SizedBox(
+            height: 100,
           ),
         ],
       ),
     );
-
   }
-  void dispose(){
-    _controller.dispose();
-    super.dispose();
+}
+
+class NewPage extends StatelessWidget {
+  const NewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: FluttermojiCircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.grey[200],
+                ),
+              ),
+              SizedBox(
+                width: min(600, _width * 0.85),
+                child: Row(
+                  children: [
+                    Text(
+                      "Customize:",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Spacer(),
+                    FluttermojiSaveWidget(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 8.0, vertical: 30),
+                child: FluttermojiCustomizer(
+                  scaffoldWidth: min(600, _width * 0.85),
+                  autosave: false,
+                  theme: FluttermojiThemeData(
+                      boxDecoration: BoxDecoration(boxShadow: [BoxShadow()])),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
